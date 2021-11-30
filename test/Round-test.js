6eb470da33c11f053581b20082a44c970ecd1ab3
Ryan McBride <mr.ryan444@gmail.com>
2021-11-30 16:04:10 -0700

    Add property to class file

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,7 +16,7 @@ describe('Round', function() {
     expect(round).to.be.an.instanceOf(Round);
   });
 
-  it.skip('should have a deck of cards', function() {
+  it('should have a deck of cards', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
@@ -24,7 +24,7 @@ describe('Round', function() {
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
-    expect(round.deck).to.deep.equal(deck);
+    expect(round.deck).to.equal(deck);
   });
 
   it.skip('should return the current card', function() {
