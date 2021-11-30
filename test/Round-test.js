2101c73530cfae35418c3602e418f9d5cccea865
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 21:36:35 -0700

    Add testing for current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -25,4 +25,16 @@ describe('Round', function() {
 
     expect(round.deck).to.deep.equal(deck);
   });
+
+  it.skip('should return the current card', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const currentCard = round.returnCurrentCard();
+
+    expect(currentCard).to.equal(round.deck[0]);
+  });
 });
