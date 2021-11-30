6365992378e230e89fb78c4ddf3eac590508bed3
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 22:35:49 -0700

    Add tests for updating turns count

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 describe('Round', function() {
 
@@ -12,7 +13,7 @@ describe('Round', function() {
 
   it.skip('should be an instance of Round', function() {
     const round = new Round();
-    expect(round).to.be.an.instanceOf(round);
+    expect(round).to.be.an.instanceOf(Round);
   });
 
   it.skip('should have a deck of cards', function() {
@@ -24,11 +25,6 @@ describe('Round', function() {
     const round = new Round(deck);
 
     expect(round.deck).to.deep.equal(deck);
-  });
-
-  it.skip('should have an initial number of turns of 0', function() {
-    const round = new Round();
-    expect(round.turns).to.equal(0);
   });
 
   it.skip('should return the current card', function() {
@@ -43,5 +39,27 @@ describe('Round', function() {
     expect(currentCard).to.equal(round.deck[0]);
   });
 
+  it.skip('should start of with 0 turns', function() {
+    const round = new Round();
+    expect(round.turns).to.equal(0);
+  });
 
+  it.skip('should start off without any incorrect guesses', function() {
+    const round = new Round();
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it.skip('should update the turns count', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    
+    round.takeTurn('object');
+    round.takeTurn('array');
+
+    expect(round.turns).to.equal(2);
+  });
 });
