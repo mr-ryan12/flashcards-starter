0c116f5672167584e314965bafb8d42606e2db48
Ryan McBride <mr.ryan444@gmail.com>
2021-11-30 16:11:59 -0700

    Pass round tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -27,7 +27,7 @@ describe('Round', function() {
     expect(round.deck).to.equal(deck);
   });
 
-  it.skip('should return the current card', function() {
+  it('should return the current card', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
@@ -36,7 +36,7 @@ describe('Round', function() {
     const round = new Round(deck);
     const currentCard = round.returnCurrentCard();
 
-    expect(currentCard).to.deep.equal(round.deck[0]);
+    expect(currentCard).to.equal(round.deck.cards[0]);
   });
 
   it.skip('should start of with 0 turns', function() {
