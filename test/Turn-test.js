740b91ba48641e4db7695f552485777a68553e03
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 18:40:41 -0700

    Add tests to check currentCard property

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
@@ -13,5 +14,16 @@ describe('Turn', function() {
   it.skip('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it.skip('should store a guess', function() {
+    const turn = new Turn('object');
+    expect(turn.guess).to.equal('object');
+  });
+
+  it.skip('should store an instance of Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.currentCard).to.be.an.instanceOf(card);
   });
 });
