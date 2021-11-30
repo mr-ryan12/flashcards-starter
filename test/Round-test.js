2d65ca3998c2615ad7790155c26d0875382d6615
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 21:33:32 -0700

    Test for deck of cards

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Round', function() {
@@ -12,5 +13,16 @@ describe('Round', function() {
   it.skip('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceOf(round);
+  });
+
+  it.skip('should have a deck of cards', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.deck).to.deep.equal(deck);
   });
 });
