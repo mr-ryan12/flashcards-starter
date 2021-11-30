eb972b537bb80fa6b46f5fb1d5982e93736cbf87
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 18:46:33 -0700

    Add tests for method functionality

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -24,6 +24,23 @@ describe('Turn', function() {
   it.skip('should store an instance of Card', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
+
     expect(turn.currentCard).to.be.an.instanceOf(card);
+  });
+
+  it.skip('should return a guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    const guess = turn.returnGuess();
+
+    expect(guess).to.equal('object');
+  });
+
+  it.skip('should return a card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    const currentCard = turn.returnCard();
+
+    expect(currentCard).to.equal(card);
   });
 });
