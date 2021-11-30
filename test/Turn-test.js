057fc6ce4cb1753d8351f51cbe09d74bbbdc337a
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 19:50:20 -0700

    Add method to pass turn test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -28,7 +28,7 @@ describe('Turn', function() {
     expect(turn.currentCard).to.equal(card);
   });
 
-  it.skip('should return a guess', function() {
+  it('should return a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     const guess = turn.returnGuess();
