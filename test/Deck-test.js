314fe2205c66923a9fb1156b9e390f5a77280853
Ryan McBride <mr.ryan444@gmail.com>
2021-11-29 21:09:17 -0700

    Add property to pass deck test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -10,9 +10,19 @@ describe('Deck', function() {
     expect(Deck).to.be.a('function')
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceOf(Deck);
+  });
+
+  it('should be able to store cards', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it.skip('should return the number of cards in the deck', function() {
