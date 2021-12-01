643844c9b9e6e18b531cf0e9b892305e7c336129
Ryan McBride <mr.ryan444@gmail.com>
2021-11-30 21:28:42 -0700

    Remove commented out code

--- src/Game.js
+++ src/Game.js
@@ -26,8 +26,8 @@ class Game {
     const round = new Round(deck);
 
     this.currentRound = round;
-    // this.printMessage(deck, round);
-    // this.printQuestion(round);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
 }
 
