9931760512bd7c012505d69e6e8f7c24264115a3
Ryan McBride <mr.ryan444@gmail.com>
2021-12-01 16:40:55 -0700

    Reorder variables

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
-
-const Card = require('../src/Card');
 const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
+const Card = require('../src/Card');
+
 
 describe('Card', function() {
 
