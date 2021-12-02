7c09be7ea66e5a098785c0704c9910db23a91093
Ryan McBride <mr.ryan444@gmail.com>
2021-12-01 17:21:37 -0700

    Refactor tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,37 +2,37 @@ const chai = require('chai');
 const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 describe('Deck', function() {
-  
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+
+  beforeEach(function() {
+    card1 = prototypeQuestions[0];
+    card2 = prototypeQuestions[1];
+    card3 = prototypeQuestions[2];
+    
+    deck = new Deck([card1, card2, card3]);
+  });
+
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function')
   });
 
   it('should be an instance of Deck', function() {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceOf(Deck);
   });
 
   it('should be able to store cards', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
-    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-    
-    const deck = new Deck([card1, card2, card3]);
-
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should return the number of cards in the deck', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
-    const card3 = new Card(3, 'What type of protoype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-
-    const deck = new Deck([card1, card2, card3]);
     const cardCount = deck.countCards();
-
     expect(cardCount).to.equal(deck.cards.length);
   });
 });
